562d2f7a2548ebb8b8e2f19283cbf96be0cf232a
kaptan58 <albaylogistik@hotmail.com>
2025-06-11 07:55:43 +0200

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'Arial, sans-serif', padding: '2rem' }}>
+      <h1>🚖 VEIGO Admin Panel</h1>
+      <p>Dashboard is working!</p>
+    </div>
+  );
+}
